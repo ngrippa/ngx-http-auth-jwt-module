3a2b13aff347c223fab90ff52047b9fcc5a0acda
Nemo Grippa <nemo.grippa@gmail.com>
2021-02-25 14:24:00 +0100

    log data

--- src/ngx_http_auth_jwt_module.c
+++ src/ngx_http_auth_jwt_module.c
@@ -419,7 +419,7 @@ static char * getJwt(ngx_http_request_t *r, ngx_str_t auth_jwt_validation_type)
 //    if (r->args->len != 0) {
 //
 //    }
-	/*if (auth_jwt_validation_type.len == 0 || (auth_jwt_validation_type.len == sizeof("AUTHORIZATION") - 1 && ngx_strncmp(auth_jwt_validation_type.data, "AUTHORIZATION", sizeof("AUTHORIZATION") - 1)==0))
+	if (auth_jwt_validation_type.len == 0 || (auth_jwt_validation_type.len == sizeof("AUTHORIZATION") - 1 && ngx_strncmp(auth_jwt_validation_type.data, "AUTHORIZATION", sizeof("AUTHORIZATION") - 1)==0))
 	{
 		// using authorization header
 		authorizationHeader = search_headers_in(r, authorizationHeaderName.data, authorizationHeaderName.len);
@@ -447,7 +447,7 @@ static char * getJwt(ngx_http_request_t *r, ngx_str_t auth_jwt_validation_type)
 		{
 			jwtCookieValChrPtr = ngx_str_t_to_char_ptr(r->pool, jwtCookieVal);
 		}
-	}*/
+	}
 
 	return jwtCookieValChrPtr;
 }
