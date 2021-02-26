a83beb568699f30e2252b01024ff431cc3d019e2
Nemo Grippa <nemo.grippa@gmail.com>
2021-02-26 09:35:57 +0100

    Free memory

--- src/ngx_http_auth_jwt_module.c
+++ src/ngx_http_auth_jwt_module.c
@@ -415,7 +415,8 @@ static char * getJwt(ngx_http_request_t *r, ngx_str_t auth_jwt_validation_type)
 	ngx_int_t n;
 	ngx_str_t authorizationHeaderStr;
 	ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "auth_jwt_validation_type.len %d", auth_jwt_validation_type.len);
-    if (r->args && r->args.len > 5) {
+    ngx_log_error(NGX_LOG_ERROR, r->connection->log, 0, "Args are %i, %s", r->args.len, r->args.data);
+	if (r->args.len > 5) {
         char *query = strdup ((const char *) r->args.data),
                 *tokens = query,
                 *p = query;
